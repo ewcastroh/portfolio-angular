ae38f4fb78b2a0bd8ac8fa6016eedb2c35d93a1f
Eimer Castro <ewcastroh10@gmail.com>
2018-09-09 10:57:33 -0500

    Fin sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { Producto } from '../interfaces/producto.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +10,32 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: Producto[] = [];
 
-  constructor( private http: HttpClient ) {
-
+  constructor(private http: HttpClient) {
     console.log('Servicio de infoPagina listo.');
 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     // Leer Archivo JSON
-    this.http.get('assets/data/data-pagina.json')
+    this.http
+      .get('assets/data/data-pagina.json')
       .subscribe((resp: InfoPagina) => {
         console.log(resp);
         // console.log(resp['email']);
         this.info = resp;
+        this.cargada = true;
+      });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://html-angular-76e22.firebaseio.com/equipo.json')
+      .subscribe((resp: Producto[]) => {
+        // console.log(resp);
+        this.equipo = resp;
         this.cargada = true;
       });
   }
